42468053db8aa29332e5725aa4040b7ae9f57b15
Emzy K <ekralik@student.tgm.ac.at>
2025-09-20 20:01:38 +0200

    basic design structure done, new updates to view.py to match the updated design.ui

--- view.py
+++ view.py
@@ -8,28 +8,21 @@
 # • Neuen Spielzug auswählen: (Schere, Stein und Papier)
 # • Neuen Spielzug ausführen
 import sys
-from PyQt6.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton
+from PyQt6.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QWidget
 from PyQt6 import uic
 
 class View(QMainWindow):
-    lValue: QLabel
-    lAvg: QLabel
-    pBGo: QPushButton
-    pBReset: QPushButton
-
     def __init__(self):
         super().__init__()
         uic.loadUi("design.ui", self)
-        # self.pBGo.clicked.connect(c.submit)
-        # self.pBReset.clicked.connect(c.reset)
-        self.pBReset.clicked.connect(self.reset)  # testhalber
+        self.setWindowTitle("Schere, Stein, Papier")
         self.reset()
 
     def setValues(self, value: int, count: int, avg: float):
         pass
 
     def setTxtStatusbar(self, s: str) -> None:
-        self.statusbar.showMessage(s)
+        self.statusBar().showMessage(s)
 
     def reset(self) -> None:
         self.setValues(0, 0, None)
@@ -38,4 +31,4 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     view = View()
     view.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
